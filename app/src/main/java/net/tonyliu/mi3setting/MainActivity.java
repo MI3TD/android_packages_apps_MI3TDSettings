a20e9ca34fccea55c074a5fd943ef8314987529e
szlytlyt <szlytlyt@outlook.com>
2016-02-07 13:46:12 +0800

    V1.1 更改：1.采用 SystemProperties 来实现对系统变量的更改 2.更改切换的控件 采用RadioButton

--- app/src/main/java/net/tonyliu/mi3setting/MainActivity.java
+++ app/src/main/java/net/tonyliu/mi3setting/MainActivity.java
@@ -1,40 +1,65 @@
 package net.tonyliu.mi3setting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.os.SystemProperties;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textview = (TextView)findViewById(R.id.textView);
-        textview.setText("123"+getMicMode());
-    }
+        final RadioButton mic1 = (RadioButton) findViewById(R.id.Mic1);
+        final RadioButton mic2 = (RadioButton) findViewById(R.id.Mic2);
+        //开始读取MicMode
+        switch (getMicMode()){
+            case 1:
+                mic1.setChecked(true);
+                mic2.setChecked(false);
+            case 2:
+                mic2.setChecked(true);
+                mic1.setChecked(false);
+        }
+        RadioGroup group = (RadioGroup)this.findViewById(R.id.raidoGroup);
+        //绑定一个匿名监听器
+        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup arg0, int arg1) {
+                // TODO Auto-generated method stub
+                //获取变更后的选中项的ID
+                int radioButtonId = arg0.getCheckedRadioButtonId();
+                //根据ID获取RadioButton的实例
+                RadioButton rb = (RadioButton)MainActivity.this.findViewById(radioButtonId);
+                //通过判断对象来设置模式
+                if(rb == mic1){
+                    setMicMode(1);
+                }if (rb == mic2){
+                    setMicMode(2);
+                }else {
+//nothing
+                }
+
+            }
+        });
+        }
     //=======================MicMode=======================
 
-    public String getMicMode(){ //获取MicMode值
-        return System.getProperty ("persist.audio.vns.mode");
-    }
-    public String setMicMode(int val){//更改MicMode值
-        return System.setProperty("persist.audio.vns.mode", String.valueOf(val));
-    }
-    public boolean cutoverMicMode(){//切换MicMode值
-        try{
-            switch (getMicMode()){
-                case "1":
-                    setMicMode(2);
-                    break;
-                case "2":
-                    setMicMode(1);
-                    break;
-                default :
-                    setMicMode(2);
-                    break;
-            }
-            return true;
-        }catch (Exception e){
-            return false;
+    public int getMicMode() { //获取MicMode值
+        switch (SystemProperties.get("persist.audio.vns.mode")){
+            case "1":
+                return 1;
+            case "2":
+                return 2;
+            default :
+                setMicMode(2);
+                return 2;
         }
     }
 
+    public void setMicMode(int val) {//更改MicMode值
+            SystemProperties.set("persist.audio.vns.mode",String.valueOf(val));
+    }
+    //=======================MicMode=======================
 }
+
