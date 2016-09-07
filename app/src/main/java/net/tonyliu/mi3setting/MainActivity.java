931ca29c44c0538f7c53d7848a93eaa624ad07f7
Xuefer <xuefer@gmail.com>
2016-09-07 10:02:46 +0800

    fix permission, remove unused import

--- app/src/main/java/net/tonyliu/mi3setting/MainActivity.java
+++ app/src/main/java/net/tonyliu/mi3setting/MainActivity.java
@@ -1,13 +1,13 @@
 package net.tonyliu.mi3setting;
 
 import android.os.Bundle;
-import android.os.SystemProperties;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+
 
 import java.io.IOException;
 
