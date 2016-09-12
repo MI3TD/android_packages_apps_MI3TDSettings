ecd1fbbc6e7d80e901c27ba840769c165034c865
Xuefer <xuefer@gmail.com>
2016-09-12 22:02:59 +0800

    move preference check to Boot Receiver

--- app/src/main/java/net/tonyliu/mi3setting/BasebandAutoResetter.java
+++ app/src/main/java/net/tonyliu/mi3setting/BasebandAutoResetter.java
@@ -3,11 +3,9 @@ package net.tonyliu.mi3setting;
 import android.annotation.Nullable;
 import android.app.Service;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
-import android.preference.PreferenceManager;
 
 
 public class BasebandAutoResetter extends Service {
@@ -22,23 +20,19 @@ public class BasebandAutoResetter extends Service {
         return null;
     }
 
-    private static final String KEY_RIL_AUTO_RESET = "ril_auto_rest";
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        if (sharedPreferences.getBoolean(KEY_RIL_AUTO_RESET , true)) {
-            handler_.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    String radioVersion = Build.getRadioVersion();
-                    BasebandAutoResetter self = BasebandAutoResetter.this;
-                    if (radioVersion.length() == 0) {
-                        Helper.resetBaseband(self);
-                    }
-                    self.stopSelf();
+        handler_.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                String radioVersion = Build.getRadioVersion();
+                BasebandAutoResetter self = BasebandAutoResetter.this;
+                if (radioVersion.length() == 0) {
+                    Helper.resetBaseband(self);
                 }
-            }, RESET_DELAY * 1000);
-        }
+                self.stopSelf();
+            }
+        }, RESET_DELAY * 1000);
         return super.onStartCommand(intent, flags, startId);
     }
 }
--- app/src/main/java/net/tonyliu/mi3setting/BootCompletedReceiver.java
+++ app/src/main/java/net/tonyliu/mi3setting/BootCompletedReceiver.java
@@ -3,12 +3,19 @@ package net.tonyliu.mi3setting;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
+    private static final String KEY_RIL_AUTO_RESET = "ril_auto_rest";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        context.startService(new Intent(context, BasebandAutoResetter.class));
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if (sharedPreferences.getBoolean(KEY_RIL_AUTO_RESET , true)) {
+            context.startService(new Intent(context, BasebandAutoResetter.class));
+        }
     }
 }
 
