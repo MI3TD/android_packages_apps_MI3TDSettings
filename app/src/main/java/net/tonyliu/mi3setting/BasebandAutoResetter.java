031ad6a1e4ef93ded75d3eab9fbc72bfe6a8223b
Xuefer <xuefer@gmail.com>
2016-09-12 17:48:48 +0800

    add switch to enable/disable auto resetter
    Signed-off-by: Xuefer <xuefer@gmail.com>

--- app/src/main/java/net/tonyliu/mi3setting/BasebandAutoResetter.java
+++ app/src/main/java/net/tonyliu/mi3setting/BasebandAutoResetter.java
@@ -3,9 +3,11 @@ package net.tonyliu.mi3setting;
 import android.annotation.Nullable;
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 
 
 public class BasebandAutoResetter extends Service {
@@ -20,19 +22,23 @@ public class BasebandAutoResetter extends Service {
         return null;
     }
 
+    private static final String KEY_RIL_AUTO_RESET = "ril_auto_rest";
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        handler_.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                String radioVersion = Build.getRadioVersion();
-                BasebandAutoResetter self = BasebandAutoResetter.this;
-                if (radioVersion.length() == 0) {
-                    Helper.resetBaseband(self);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (sharedPreferences.getBoolean(KEY_RIL_AUTO_RESET , true)) {
+            handler_.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    String radioVersion = Build.getRadioVersion();
+                    BasebandAutoResetter self = BasebandAutoResetter.this;
+                    if (radioVersion.length() == 0) {
+                        Helper.resetBaseband(self);
+                    }
+                    self.stopSelf();
                 }
-                self.stopSelf();
-            }
-        }, RESET_DELAY * 1000);
+            }, RESET_DELAY * 1000);
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 }
